dfc0a6704823bec30eaea8526f4a2b7b9e560571
cgrundman <cgrundman.grundman@gmail.com>
2025-04-11 13:55:15 +0200

    display all weight data in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { WEIGHT_DATA } from './data/weight-data';
+import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'weight-tracker';
+  weightData = WEIGHT_DATA;
 }
